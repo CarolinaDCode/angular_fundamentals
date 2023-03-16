8209f7f55fc811329f6e02c970d07e6ecf75bf8e
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-03-15 23:51:27 -0500

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'diana';
+  name = 'Diana';
   age = 20;
   img = 'https://images.hola.com/imagenes/mascotas/20210217184541/gatos-gestos-lenguaje-significado/0-922-380/gatos-gestos-m.jpg?tx=w_680';
+  btnDisabled = true;
+  person = {
+    name: 'Vanessa',
+    age: 32,
+    avatar: 'https://hips.hearstapps.com/vidthumb/images/vanessa-hudgens-1670836622.jpg'
+  }
 }
