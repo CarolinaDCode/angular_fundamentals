fb334734d37f9b95e5603645319592c206ca907e
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-03-16 23:22:59 -0500

    eventos click y KeyUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'Diana';
   age = 20;
-  img = 'https://images.hola.com/imagenes/mascotas/20210217184541/gatos-gestos-lenguaje-significado/0-922-380/gatos-gestos-m.jpg?tx=w_680';
+  img = "https://images.hola.com/imagenes/mascotas/20210217184541/gatos-gestos-lenguaje-significado/0-922-380/gatos-gestos-m.jpg?tx=w_680";
   btnDisabled = true;
   person = {
     name: 'Vanessa',
@@ -22,5 +22,15 @@ export class AppComponent {
 
   increaseAge(){
     this.person.age += 1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
   }
 }
