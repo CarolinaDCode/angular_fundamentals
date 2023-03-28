224dd297f0cc8540366d3276015aca0151a34c50
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-03-28 00:06:14 -0500

    first part, component life cycle, constructor

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.css']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit, OnChanges{
 
-  @Input() img: string = 'valor init';
+  @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
 
   imageDefault = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ-qA-E6jcwhgYe5Nw3ClDO4RqCQuj9Dj9S45wVlzGqeBJmfCuNTgtcBC23Pf9BsAyUoY&usqp=CAU";
 
-  constructor() {}
+  constructor() {
+    /** Ciclo de vida de componentes:(1)
+     * Before Render -> Corre antes del Render
+     - Y Al final angular necesita de este componente y crear la instancia de ese componente para reutilizarlo,
+     - Es importante que en el constructor no corras cosas asincronas (petición ,fetch, suscripción), solo aquelas a la que le asignas un valor
+     inmediato.
+     - async corre solo una vez, pero la instancia del constructor se creará cada vez que creemos este componente, entonces No utilizar async.
+    */
+    console.log('constructor', 'imgValue =>', this.img)
+  }
+
+  ngOnChanges(){
+      
+  }
 
   ngOnInit(): void {
   }
