175d6c23bed541e33a21b4e82415a6d8e69b939a
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-04-14 00:15:39 -0500

    add observable myCart

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject} from 'rxjs';
+
 import { Product } from '../models/product.model';
 
+//La libreia rxjs, es la que implemente todo el patron de observables dentro de angular; es algo que viene en el núcleo en las bases del core de angular (los vez en formularios, peticiones http).
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
   private myShoppingCart: Product[] = [];
+  private myCart = new BehaviorSubject<Product[]>([]);
 
   constructor() { }
 
